c4c3e7d6dc6ac641f8156adb6bcdbcb64fc7ba84
2009117 <78092565+2009117@users.noreply.github.com>
2021-06-23 22:48:57 -0500

    Update Chapter6.ex05Chris.c

--- Chapter6.ex05Chris.c
+++ Chapter6.ex05Chris.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int Y; //This is the variable to calculate if is a leap year
+char on[1000];
 
 int main(void) {
+
 printf("Check if the year is a leap year\n") ;
 printf("Insert a year\n");
-scanf("%d", &Y);
+fgets(on,sizeof(on),stdin);
+sscanf(on,"%d", &Y);
 
 //Check if the year is divisible by 400
 if (Y/400 ==0) {
