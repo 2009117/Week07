1aa1aba62dc419c8f9c74eeec79d48b417c1daea
2009117 <78092565+2009117@users.noreply.github.com>
2021-06-23 21:20:26 -0500

    Update Chapter6.ex01Chris.c

--- Chapter6.ex01Chris.c
+++ Chapter6.ex01Chris.c
@@ -4,23 +4,30 @@
 float X1, X2;//These are the variables for the first point
 float Y1, Y2;//These are the variables for the 2do point
 float D; //This is the variable for the distance beetwen 2 points.
+char at[1000];
 
 int main(void) {
 
 printf("Insertar X1\n");
-scanf("%f", &X1);
+fgets(at,sizeof(at),stdin);
+sscanf(at, "%f", &X1);
 
 
 printf("Insertar X2\n");
-scanf("%f", &X2);
+fgets(at,sizeof(at),stdin);
+sscanf(at, "%f", &X2);
+
 
 
 printf("Insertar Y1\n");
-scanf("%f", &Y1);
+fgets(at,sizeof(at),stdin);
+sscanf(at, "%f", &Y1);
 
 
 printf("Insertar Y2\n");
-scanf("%f", &Y2);
+fgets(at,sizeof(at),stdin);
+sscanf(at, "%f", &Y2);
+
 
 D=sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
 
