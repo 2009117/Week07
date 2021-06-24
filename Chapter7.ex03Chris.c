5a5e2ad50bd0392fe939b52cdcf34b2ccb1f9e1c
2009117 <78092565+2009117@users.noreply.github.com>
2021-06-23 23:03:44 -0500

    Update Chapter7.ex03Chris.c

--- Chapter7.ex03Chris.c
+++ Chapter7.ex03Chris.c
@@ -5,9 +5,12 @@ float X; //Varibale to insert the weight of the file
 float H; //Hours
 float D; //Days
 
+char on[1000];
+
 int main(void) {
   printf("Introduce the weight of your file\n ");
-  scanf("%f",&X);
+  fgets(on, sizeof(on),stdin);
+  sscanf(on,"%f",&X);
     //To know the hours
     H=(1*X)/(34560000);
     printf("The time that your file is gonna send is: %f\n",H);
