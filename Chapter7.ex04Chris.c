a5808d096c011cac0f94a510ca4e99845071dd90
2009117 <78092565+2009117@users.noreply.github.com>
2021-06-23 23:05:14 -0500

    Update Chapter7.ex04Chris.c

--- Chapter7.ex04Chris.c
+++ Chapter7.ex04Chris.c
@@ -4,9 +4,13 @@
 float G; //Ganancia
 float NG; //Nueva ganancia 
 
+char on[1000];
+
 int main(void) {
   printf("Insert the amout of money that you won\n");
-  scanf("%f",&G);
+  fgets(on, sizeof(on), stdin);
+  sscanf(on,"%f",&G);
+  
   //This is the operation to calculate the sales tax.
     NG=G*.08;
   printf("The amout that you need to pay is $%.2f", NG);
